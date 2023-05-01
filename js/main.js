ffc02197b9056d6e301da9dcd48bab1d52bc742f
DamnedFR34K <108131700+DamnedFR34K@users.noreply.github.com>
2023-05-01 13:18:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+
  const ELIMINA = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="color: red;" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"/></svg>'
  const CREA = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="color: green;font-size:20px;" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/></svg> '
  const MODIFICA = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16"><path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/></svg>'
@@ -1595,25 +1596,27 @@ output.addEventListener("mousedown", function(evento) {
     const distanza = Math.sqrt(Math.pow((puntoRilascio.x - puntoPressione.x), 2) + Math.pow((puntoRilascio.y - puntoPressione.y), 2));
     
     const sogliaVerticale = 50;
+    let newChoice = choice;
     if (distanza >= 150 && percorsi && percorsi > 1 && choice >= 0 && choice < percorsi) {
       if (Math.abs(puntoRilascio.y - puntoPressione.y) < sogliaVerticale) {
         if (puntoRilascio.x > puntoPressione.x) {
           spostamentoDestra = true;
           if (choice == 0) {
-            choice = percorsi - 1;
+            newChoice = percorsi - 1;
           } else {
-            choice--;
+            newChoice--;
           }
         } else if (puntoRilascio.x < puntoPressione.x) {
           spostamentoDestra = false;
           if (choice == percorsi - 1) {
-            choice = 0;
+            newChoice = 0;
           } else {
-            choice++;
+            newChoice++;
           }
         }
       }
-      document.getElementById("output").innerHTML = risultato();
+      changeChoice(newChoice)
+     
     }
   });
   
@@ -1623,22 +1626,24 @@ output.addEventListener("mousedown", function(evento) {
     const distanza = Math.sqrt(Math.pow((puntoRilascio.x - puntoPressione.x), 2) + Math.pow((puntoRilascio.y - puntoPressione.y), 2));
   
  const sogliaVerticale = 50;
+ let newChoice = choice;
  if (distanza >= 150 && percorsi > 1) {
 
     if (Math.abs(puntoRilascio.y - puntoPressione.y) < sogliaVerticale) {
     if (puntoRilascio.x > puntoPressione.x) {
         spostamentoDestra = true;
-        if(choice == 0)choice = percorsi-1;
-        else choice--;
+        if(choice == 0)newChoice = percorsi-1;
+        else newChoice--;
        
       } else if (puntoRilascio.x < puntoPressione.x) {
         spostamentoDestra = false;
-        if(choice == percorsi-1)choice = 0;
-        else choice++;
+        if(choice == percorsi-1)newChoice = 0;
+        else newChoice++;
       }
     }
     
-        document.getElementById("output").innerHTML =risultato()
+    changeChoice(newChoice)
+     
     
  } 
  });
